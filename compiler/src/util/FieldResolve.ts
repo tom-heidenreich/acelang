0bba28ce43e3fe3fb655f0a6133f13175bf19e79
polycloud-dev <heidenreichdd@gmail.com>
2022-12-25 13:45:32 +0100

    added reference resolver

--- compiler/src/util/FieldResolve.ts
+++ compiler/src/util/FieldResolve.ts
@@ -1,15 +1,6 @@
 import { Field, FieldInstructions, Fields, Functions, Param } from "../types";
 
 export default class FieldResolve {
-    
-    private static searchFunctions(functions: Functions, name: string) {
-        const func = functions[name];
-        if(func) {
-            return {
-                type: func.returnType
-            }
-        }
-    }
 
     public static resolve(fields: FieldInstructions, name: string, functions?: Functions): Field | undefined {
 
@@ -28,5 +19,20 @@ export default class FieldResolve {
         }
 
         return undefined;
+    }
+
+    public static resolveReferences(field: Field, fields: FieldInstructions): string | undefined {
+        if(field.reference) {
+            const ref = FieldResolve.resolve(fields, field.reference);
+            if(ref) {
+                const resolvedRef = FieldResolve.resolveReferences(ref, fields);
+                if(resolvedRef) return resolvedRef;
+            }
+            else {
+                throw new Error(`Reference ${field.reference} not found`);
+            }
+            return field.reference;
+        }
+        return;
     }
 }
