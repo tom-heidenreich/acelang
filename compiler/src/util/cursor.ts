30a68bdd26b802c0461d29a9b5a905c94e38cd5e
polycloud-dev <heidenreichdd@gmail.com>
2022-12-25 13:15:20 +0100

    improved cursor

--- compiler/src/util/cursor.ts
+++ compiler/src/util/cursor.ts
@@ -4,7 +4,7 @@ export default class Cursor<T> {
     private readonly array: T[];
 
     constructor(array: T[], offset?: number) {
-        this.array = array;
+        this.array = array || [];
         if(offset) this.cursor = offset;
     }
 
@@ -26,5 +26,50 @@ export default class Cursor<T> {
 
     public reachedEnd() {
         return this.cursor >= this.array.length;
+    }
+
+    public remaining(): Cursor<T> {
+        const cursor = this.cursor;
+        this.cursor = this.array.length;
+        return new Cursor(this.array, cursor);
+    }
+
+    public asList(): T[] {
+        return this.array.slice(this.cursor);
+    }
+}
+
+export class WriteCursor<T> {
+
+    private cursor = 0;
+    private readonly array: T[];
+
+    constructor(array: T[], offset?: number) {
+        this.array = array || [];
+        if(offset) this.cursor = offset;
+    }
+
+    public push(...values: T[]) {
+        for(const value of values) {
+            this.array[this.cursor++] = value;
+        }
+    }
+
+    public rollback() {
+        if(this.cursor <= 0) throw new Error('Cursor out of bounds');
+        this.cursor--;
+        return this.array[this.cursor];
+    }
+
+    public asList(): T[] {
+        return this.array.slice(0, this.cursor);
+    }
+
+    public clear() {
+        this.cursor = 0;
+    }
+
+    public size() {
+        return this.cursor;
     }
 }
