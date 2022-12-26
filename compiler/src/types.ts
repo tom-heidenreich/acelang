98f8bb52c03263256dd479d393190289c87ab263
polycloud-dev <heidenreichdd@gmail.com>
2022-12-26 19:42:27 +0100

    sorted and added types and values

--- compiler/src/types.ts
+++ compiler/src/types.ts
@@ -1,6 +1,6 @@
 export type DataType = 'string' | 'int' | 'float' | 'void' | 'unknown' | 'any' | 'callable';
-export type Keyword = 'const' | 'var' | 'func' | 'sync' | 'return';
-export type Symbol = '=' | '+' | '-' | '*' | '/' | '>' | '<' | '^' | '%' | ':' | ',' | '.'
+export type Keyword = 'const' | 'var' | 'func' | 'sync' | 'return' | 'type';
+export type Symbol = '=' | '+' | '-' | '*' | '/' | '>' | '<' | '^' | '%' | ':' | ',' | '.' | '|'
 
 export type Token = {
     value: string;
@@ -10,52 +10,28 @@ export type Token = {
 }
 
 export const DATATYPES: DataType[] = ['string', 'int', 'float', 'void', 'any']
-export const KEYWORDS: Keyword[] = ['const', 'var', 'func', 'sync', 'return']
-export const SYMBOLS: Symbol[] = ['=', '+', '-', '*', '/', '>', '<', '^', '%', ':', ',', '.']
+export const KEYWORDS: Keyword[] = ['const', 'var', 'func', 'sync', 'return', 'type']
+export const SYMBOLS: Symbol[] = ['=', '+', '-', '*', '/', '>', '<', '^', '%', ':', ',', '.', '|']
 
-export type Value = string | number;
 export type Identifier = string;
+export type Primitive = string | number | boolean;
 
-export type Call = {
-    type: 'call',
-    name: string,
-    args: Argument[]
-}
-
-export type Step = (Identifier | Value | Symbol | Call)
-
-export type Steps = {
-    type: 'steps',
-    value: Step[]
-}
-
-export type ValueType = 'value' | 'steps' | 'reference'
-
-export type Argument = {
-    type: 'argument',
-    dataType: DataType,
-    value: Value | Steps,
-    valueType: ValueType
-}
-
+// keywords
 export type Const = {
     type: 'const',
     name: string,
-    value: Value | Steps,
-    valueType: ValueType
+    value: Value,
 }
 
 export type Var = {
     type: 'var',
     name: string,
-    value?: Value | Steps,
-    valueType: ValueType
+    value?: Value,
 }
 
 export type Return = {
     type: 'return',
-    value?: Value | Steps,
-    valueType: ValueType
+    value?: Value,
 }
 
 export type Sync = {
@@ -63,14 +39,15 @@ export type Sync = {
     instructions: Instructions,
 }
 
+// functions
 export type Param = {
     name: string,
-    type: DataType,
+    type: Type,
 }
 
 export type Function = {
     params: Param[],
-    returnType: DataType,
+    returnType: Type,
     body: Instructions,
     isSync: boolean,
 }
@@ -79,19 +56,9 @@ export type Functions = {
     [name: string]: Function,
 }
 
-export type Build = {
-    functions: Functions,
-    types: DataType[],
-    main: Instructions,
-}
-
-export type Instructions = {
-    fields: FieldInstructions,
-    run: (Const | Var | Sync | Return | Steps)[],
-}
-
+// fields
 export type Field = {
-    type: DataType,
+    type: Type,
     reference?: Identifier,
 }
 
@@ -102,4 +69,101 @@ export type Fields = {
 export type FieldInstructions = {
     parent?: FieldInstructions,
     local: Fields,
+}
+
+// types
+export type ReferenceType = {
+    type: 'reference',
+    reference: Identifier
+}
+
+export type LiteralType = {
+    type: 'literal',
+    literal: Primitive
+}
+
+export type UnionType = {
+    type: 'union',
+    oneOf: Type[]
+}
+
+export type StructType = {
+    type: 'struct',
+    properties: Types
+}
+
+export type ArrayType = {
+    type: 'array',
+    items: Type
+}
+
+export type PrimitiveType = {
+    type: 'primitive',
+    primitive: DataType
+}
+
+export type Type = (PrimitiveType | UnionType | StructType | ArrayType | LiteralType | ReferenceType)
+
+export type Types = {
+    [name: string]: Type,
+}
+
+// values
+export type Value = (PrimitiveValue | Steps | Reference | Struct | ArrayValue)
+export type ValueResult = {
+    value: Value,
+    type: Type,
+}
+
+export type PrimitiveValue = {
+    type: 'primitive',
+    primitive: Primitive
+};
+
+export type Reference = {
+    type: 'reference',
+    reference: string,
+}
+
+// steps
+export type Steps = {
+    type: 'steps',
+    value: Step[]
+}
+export type Step = (Identifier | PrimitiveValue | Symbol | Call)
+
+export type Call = {
+    type: 'call',
+    name: string,
+    args: Argument[]
+}
+
+
+export type Argument = {
+    type: 'argument',
+    dataType: Type,
+    value: Value
+}
+
+// objects
+export type Struct = {
+    type: 'struct',
+    properties: {[name: string]: Value},
+}
+
+export type ArrayValue = {
+    type: 'array',
+    items: Value[],
+}
+
+// build
+export type Build = {
+    functions: Functions,
+    types: Types,
+    main: Instructions,
+}
+
+export type Instructions = {
+    fields: FieldInstructions,
+    run: (Const | Var | Sync | Return | Steps)[],
 }
