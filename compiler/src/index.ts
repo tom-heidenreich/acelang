9a5c11c7ce45280b25fb4fd0274347f6d7815269
polycloud-dev <heidenreichdd@gmail.com>
2023-01-20 14:05:09 +0100

    fixed names

--- compiler/src/index.ts
+++ compiler/src/index.ts
@@ -1,23 +1,18 @@
 import path from 'path';
 import * as fs from 'fs';
-import { parse } from './parser';
-import { buildAST } from './ast-builder';
-import Runtime from './runtime';
+import { parse } from './lexer';
+import { parseToTree } from './parser';
 
  // read the file
  const content = fs.readFileSync(path.join(__dirname, '..', '..', 'index.ace'), 'utf8');
 
-const parsed = parse(content)
+const tokens = parse(content)
 
-fs.writeFileSync('./log/parsed.json', JSON.stringify(parsed, null, 4), 'utf8');
+fs.writeFileSync('./log/tokens.json', JSON.stringify(tokens, null, 4), 'utf8');
 
 // get ast
-const { tree, map } = buildAST(parsed);
+const { tree, map } = parseToTree(tokens);
 
-fs.writeFileSync('./log/tree.json', JSON.stringify(tree, null, 4), 'utf8');
+fs.writeFileSync('./log/parsed.json', JSON.stringify(tree, null, 4), 'utf8');
 
 fs.writeFileSync('./log/type_map.json', JSON.stringify(map, null, 4), 'utf8');
-
-// // runtime
-// const runtime = new Runtime(tree);
-// runtime.run();
---
+++ compiler/src/lexer.ts
@@ -0,0 +1,191 @@
+import StringBuffer from './util/buffer';
+import { DataType, Keyword, KEYWORDS, SYMBOLS, Token, Symbol, OPERATORS, Operator, TokenType } from './types';
+
+// TODO: refactor whole file
+
+function pushBuffer(line: Token[], buffer: StringBuffer, type?: 'datatype' | 'symbol', specificType?: DataType) {
+    if(!buffer.isEmpty()) {
+        const value = buffer.clear()
+        if(value.trim() === '') return;
+        let exType: TokenType | undefined = type;
+        if(type === 'symbol') {
+            // check if it's an operator
+            if(OPERATORS.includes(value as Operator)) {
+                exType = 'operator';
+            }
+        }
+        else exType = KEYWORDS.includes(value as Keyword) ? 'keyword' : !type ? 'identifier' : type;
+        if(!exType) throw new Error(`Unknown token type: ${value}`)
+
+        line.push({
+            value,
+            type: exType,
+            specificType
+        });
+    }
+}
+
+export function parse(content: string, inBlock: boolean = false) {
+
+    const result: Token[][] = []
+    const line: Token[] = []
+
+    let buffer = new StringBuffer();
+
+    let structure: 'string' | 'int' | 'float' | 'comment' | 'block' | 'symbol' | undefined;
+
+    let bracketType: '(' | '{' | '[' | undefined;
+    let countParenthesis = 0;
+    let countCurlyBrackets = 0;
+    let countSquareBrackets = 0;
+
+    for(const c of content) {
+        if(structure === 'comment') {
+            if(c === '\n') structure = undefined;
+            continue;
+        }
+        if(structure === 'symbol') {
+            if(!SYMBOLS.includes(c as Symbol)) {
+                pushBuffer(line, buffer, 'symbol');
+                structure = undefined;
+            }
+            else continue;
+        }
+        if(structure === 'block') {
+            if(c === '{') countCurlyBrackets++;
+            else if(c === '(') countParenthesis++;
+            else if(c === '[') countSquareBrackets++;
+            else if(c === '}' && bracketType === '{') {
+                if(--countCurlyBrackets === 0) {
+                    bracketType = undefined;
+                    structure = undefined;
+                    line.push({
+                        value: '{}',
+                        type: 'block',
+                        block: parse(buffer.clear(), true)
+                    });
+                    continue;
+                }
+            }
+            else if(c === ')' && bracketType === '(') {
+                if(--countParenthesis === 0) {
+                    bracketType = undefined;
+                    structure = undefined;
+                    line.push({
+                        value: '()',
+                        type: 'block',
+                        block: parse(buffer.clear(), true)
+                    });
+                    continue;
+                }
+            }
+            else if(c === ']' && bracketType === '[') {
+                if(--countSquareBrackets === 0) {
+                    bracketType = undefined;
+                    structure = undefined;
+                    line.push({
+                        value: '[]',
+                        type: 'block',
+                        block: parse(buffer.clear(), true)
+                    });
+                    continue;
+                }
+            }
+            buffer.append(c);
+            continue;
+        }
+        if(c === '{') {
+            countCurlyBrackets++;
+            bracketType = '{';
+            pushBuffer(line, buffer);
+            structure = 'block';
+            continue;
+        }
+        if(c === '(') {
+            countParenthesis++;
+            bracketType = '(';
+            pushBuffer(line, buffer);
+            structure = 'block';
+            continue;
+        }
+        if(c === '[') {
+            countSquareBrackets++;
+            bracketType = '[';
+            pushBuffer(line, buffer);
+            structure = 'block';
+            continue;
+        }
+        if(c === '"') {
+            if(structure === 'string') {
+                structure = undefined;
+                pushBuffer(line, buffer, 'datatype', 'string');
+            }
+            else structure = 'string';
+            continue;
+        }
+        if(structure === 'string') {
+            buffer.append(c);
+            continue;
+        }
+        if(c === '#') {
+            structure = 'comment';
+            continue;
+        }
+        if(c === ' ' || c === '\t') {
+            if(!structure) pushBuffer(line, buffer);
+            else pushBuffer(line, buffer, 'datatype', structure);
+            structure = undefined;
+            continue;
+        }
+        if(c === '\n' || c == '\r' || c === ';' || (c === ',' && inBlock)) {
+            if(!structure) pushBuffer(line, buffer);
+            else pushBuffer(line, buffer, 'datatype', structure);
+            structure = undefined;
+            if(line.length > 0) result.push(line.splice(0));
+            continue;
+        }
+        if(structure === 'int') {
+            if(!isNaN(Number(c))) {
+                buffer.append(c);
+                continue;
+            }
+            else if(c === '.') {
+                structure = 'float';
+                buffer.append(c);
+                continue;
+            }
+            else {
+                if(!structure) pushBuffer(line, buffer);
+                else pushBuffer(line, buffer, 'datatype', structure);
+                structure = undefined;
+            }
+        }
+        if(structure === 'float') {
+            if(!isNaN(Number(c))) {
+                buffer.append(c);
+                continue;
+            }
+            else structure = undefined;
+        }
+        if(!isNaN(Number(c))) {
+            pushBuffer(line, buffer);
+            structure = 'int';
+            buffer.append(c);
+            continue;
+        }
+        if(SYMBOLS.includes(c as Symbol)) {
+            if(structure !== 'symbol') {
+                if(!structure) pushBuffer(line, buffer);
+                else pushBuffer(line, buffer, 'datatype', structure);
+                structure = 'symbol';
+            }
+            buffer.append(c);
+            continue;
+        }
+        else buffer.append(c);
+    }
+    if(structure === 'int' || structure === 'float' || structure === 'string') pushBuffer(line, buffer, 'datatype', structure)
+    else pushBuffer(line, buffer);
+    if(line.length > 0) result.push(line);
+    return result;
+}
