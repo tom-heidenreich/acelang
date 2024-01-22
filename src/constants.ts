f4f670a3990e1fcd8c84c515ec8283550f139716
tom-heidenreich <heidenreichdd@gmail.com>
2024-01-23 00:04:46 +0100

    added uanry operators

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 export const KEYWORDS = ['const', 'var', 'type', 'func', 'return'] as const
 
 export const SYMBOLS = ['+', '-', '*', '/', ':', ';', '=', '<', '>', '!', '&', '|', ',', '.'] as const;
-export const OPERATORS = [
+
+export const BINARY_OPERATORS = [
     ['+', 5],
     ['-', 5],
     ['*', 6],
@@ -18,12 +19,38 @@ export const OPERATORS = [
     ['=>', 1],
 ] as const;
 
+export const UNARY_OPERATORS = [
+    ['!', 8],
+    ['-', 8],
+] as const;
+
+export const OPERATORS = [...BINARY_OPERATORS, ...UNARY_OPERATORS] as const;
+
+export type Operator = typeof OPERATORS[number][0];
+export type BinaryOperator = typeof BINARY_OPERATORS[number][0];
+export type UnaryOperator = typeof UNARY_OPERATORS[number][0];
+
+export function isOperator(s: string): s is Operator {
+    return OPERATORS.some(o => o[0] === s);
+}
+
+export function isBinaryOperator(s: string): s is BinaryOperator {
+    return BINARY_OPERATORS.some(o => o[0] === s);
+}
+
+export function isUnaryOperator(s: string): s is UnaryOperator {
+    return UNARY_OPERATORS.some(o => o[0] === s);
+}
+
+export function getPrecendence(operator: Operator) {
+    return OPERATORS.find(o => o[0] === operator)![1];
+}
+
 export const STRING_QUOTES = ['\'', '"'] as const;
 export const BRACKETS = [['(', ')'], ['[', ']'], ['{', '}']] as const;
 
 export type Keyword = typeof KEYWORDS[number];
 export type Symbol = typeof SYMBOLS[number];
-export type Operator = typeof OPERATORS[number][0];
 export type StringQuote = typeof STRING_QUOTES[number];
 export type Bracket = typeof BRACKETS[number];
 
@@ -33,10 +60,6 @@ export function isKeyword(s: string): s is Keyword {
 
 export function isSymbol(s: string): s is Symbol {
     return SYMBOLS.includes(s as Symbol);
-}
-
-export function isOperator(s: string): s is Operator {
-    return OPERATORS.some(o => o[0] === s);
 }
 
 export function isStringQuote(s: string): s is StringQuote {
@@ -52,8 +75,4 @@ export function getClosingBracket(s: Bracket[0]): Bracket[1] {
 
 export function isBoolean(s: string): s is 'true' | 'false' {
     return s === 'true' || s === 'false';
-}
-
-export function getPrecendence(operator: Operator) {
-    return OPERATORS.find(o => o[0] === operator)![1];
 }
