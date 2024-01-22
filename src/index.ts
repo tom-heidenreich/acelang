57f14fa5aacd6531da46e7eee8721eab6bcbe747
tom-heidenreich <heidenreichdd@gmail.com>
2024-01-23 00:43:20 +0100

    added basic comilation step

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import * as fs from 'fs';
 import { Lexer } from './lexer';
 import { INITIAL_STATE } from './lexer/states';
+import parseExpression from './parser/expressions';
+import LLVMModule from './llvm-module';
+import llvm from 'llvm-bindings';
+import { Int64Type } from './parser/types';
 
 // get file path from command line arguments
 const filePath = process.argv[2];
@@ -18,3 +22,16 @@ const tokens = lexer.tokenize(file);
 
 fs.mkdirSync('out', { recursive: true });
 fs.writeFileSync('out/tokens.json', JSON.stringify(tokens, undefined, 4));
+
+// parse
+const value = parseExpression(tokens[0]);
+
+// compile
+const _module = new LLVMModule('my_module');
+
+const mainFunction = _module.createMain();
+_module.builder.SetInsertPoint(llvm.BasicBlock.Create(_module.context, 'entry', mainFunction));
+_module.builder.CreateAlloca(new Int64Type().toLLVM(_module), value.toLLVM(_module));
+_module.exitMain();
+
+fs.writeFileSync('out/llvm.ll', _module.toString());
