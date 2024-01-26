98cec98f954469897ee5fd6db05373710f2b86a0
tom-heidenreich <heidenreichdd@gmail.com>
2024-01-26 20:07:22 +0100

    added toString function to token

--- src/lexer/tokens.ts
+++ src/lexer/tokens.ts
@@ -1,18 +1,26 @@
 import { Keyword, Operator, Symbol } from "../constants";
 
-export class Token {
-
+export abstract class Token {
+    public abstract toString(): string;
 }
 
 export class KeywordToken extends Token {
     constructor(public readonly keyword: Keyword) {
         super();
     }
+
+    public toString(): string {
+        return this.keyword;
+    }
 }
 
 export class IdentifierToken extends Token {
     constructor(public readonly identifier: string) {
         super();
+    }
+
+    public toString(): string {
+        return this.identifier;
     }
 }
 
@@ -22,11 +30,19 @@ export class StringToken extends TextValueToken {
     constructor(public readonly string: string) {
         super();
     }
+
+    public toString(): string {
+        return `"${this.string}"`;
+    }
 }
 
 export class CharToken extends TextValueToken {
     constructor(public readonly char: string) {
         super();
+    }
+
+    public toString(): string {
+        return `'${this.char}'`;
     }
 }
 
@@ -35,10 +51,18 @@ export class IntegerToken extends NumberToken {
     constructor(public readonly integer: number) {
         super();
     }
+
+    public toString(): string {
+        return this.integer.toString();
+    }
 }
 export class FloatToken extends NumberToken {
     constructor(public readonly float: number) {
         super();
+    }
+
+    public toString(): string {
+        return this.float.toString();
     }
 }
 
@@ -46,11 +70,19 @@ export class BooleanToken extends Token {
     constructor(public readonly boolean: 'true' | 'false') {
         super();
     }
+
+    public toString(): string {
+        return this.boolean;
+    }
 }
 
 export class SymbolToken extends Token {
     constructor(public readonly symbol: Symbol) {
         super();
+    }
+
+    public toString(): string {
+        return this.symbol;
     }
 }
 
@@ -58,11 +90,19 @@ export class OperatorToken extends Token {
     constructor(public readonly operator: Operator) {
         super();
     }
+
+    public toString(): string {
+        return this.operator;
+    }
 }
 
 export abstract class BlockToken extends Token {
     constructor(public readonly content: Token[][]) {
         super();
+    }
+
+    public toString(): string {
+        return `(${this.content.map(tokens => tokens.join('')).join('')})`;
     }
 }
 
@@ -73,5 +113,9 @@ export class CurlyBracketToken extends BlockToken {}
 export class NewLineToken extends Token {
     constructor(public readonly prevToken: Token | undefined = undefined) {
         super();
+    }
+
+    public toString(): string {
+        return '<newline>';
     }
 }
