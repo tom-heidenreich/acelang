0d71138d1a4d3b418cafaa8d2f336b0bccd2f185
tom-heidenreich <heidenreichdd@gmail.com>
2024-01-27 14:23:54 +0100

    added var statement parser

--- src/parser/statements/index.ts
+++ src/parser/statements/index.ts
@@ -1,10 +1,12 @@
 import { KeywordToken, Token } from "../../lexer/tokens";
 import { Environment, Statement } from "../util";
 import ConstStatementParser from "./const";
+import VarStatementParser from "./var";
 
 export default function parseStatement(keyword: KeywordToken, tokens: Token[], env: Environment): Statement {
     switch(keyword.keyword) {
         case 'const': return new ConstStatementParser(tokens, env).parse();
+        case 'var': return new VarStatementParser(tokens, env).parse();
     }
     throw new SyntaxError(`Unexpected keyword ${keyword.keyword}`);
 }
